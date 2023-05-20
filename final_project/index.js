38f0fb64a2c53531329531531cb055fadbff9129
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-05-19 21:40:10 -0600

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -11,9 +11,16 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (authenticatedUser(username,password)) {
+        let accessToken = jwt.sign({
+          data: password
+        }, 'access', { expiresIn: 60 });
+        req.session.authorization = {
+          accessToken,username
+        }
+    };
 });
- 
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
