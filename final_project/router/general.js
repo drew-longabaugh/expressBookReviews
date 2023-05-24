350481b73923fcf1944c407a754bae940c2f6934
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-05-23 19:47:10 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,7 +12,7 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  res.send(JSON.stringify(books, null, 4))
+  res.send(JSON.stringify(books, null, 4));
  });
 
 // Get book details based on ISBN
@@ -22,21 +22,23 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author
-  res.send(books[author])
- });
+public_users.get('/:author',(req,res)=>{
+  const author = req.params.author;
+  let filtered_author = books.filter((books) => books.author === author);
+  res.send(filtered_author);
+ }) ;
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title
-  res.send(books[title])
+public_users.get('/title/:title', (req, res)=>{
+  const title = req.params.title;
+
+  res.send(books[title]);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  const review = req.params.review
-  res.send.apply(books[review])
+  const review = req.params.review;
+  res.send.apply(books[review]);
  });
 
 module.exports.general = public_users;
