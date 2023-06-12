d44964f8502fb8772b139cfba1843d0fa1441105
drew-longabaugh <127168479+drew-longabaugh@users.noreply.github.com>
2023-06-11 18:22:23 -0600

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -10,20 +10,43 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-    if (authenticatedUser(username,password)) {
-        let accessToken = jwt.sign({
-          data: password
-        }, 'access', { expiresIn: 60 });
-        req.session.authorization = {
-          accessToken,username
-        }
-    };
+app.post("/login", (req,res) => {
+    const user = req.body.user;
+    if (!user) {
+        return res.status(404).json({message: "Body Empty"});
+    }
+    let accessToken = jwt.sign({
+        data: user
+      }, 'access', { expiresIn: 60 * 60 });
+
+      req.session.authorization = {
+        accessToken
+    }
+    return res.status(200).send("User successfully logged in");
 });
 
+app.use("/user", (req,res,next)=>{
+   if(req.session.authorization) {
+       let token = req.session.authorization['accessToken'];
+
+       jwt.verify(token, "access",(err,user)=>{
+           if(!err){
+               req.user = user;
+               next();
+           }
+           else{
+               return res.status(403).json({message: "User not authenticated"})
+           }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"})
+    }
+});
 const PORT =5000;
 
-app.use("/customer", customer_routes);
+app.use("/customer", customer_
+        
+        s);
 app.use("/", genl_routes);
 
 app.listen(PORT,()=>console.log("Server is running"));
